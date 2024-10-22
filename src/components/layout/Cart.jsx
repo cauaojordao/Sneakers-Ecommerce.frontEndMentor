eca2b3ea0cfbacf4b48f5e4670024b405ef373cc
cauaojordao <cauaojordao@gmail.com>
2024-10-22 10:45:30 -0300

    Second Commit

--- src/components/layout/Cart.jsx
+++ src/components/layout/Cart.jsx
@@ -1,50 +1,52 @@
 import { useState, useEffect } from "react";
-import CartModal from "./CartModal";  
+import CartModal from "./CartModal";
 import cart from "../../assets/icon-cart.svg";
 import styles from "./Cart.module.css";
 
 function Cart() {
   const [cartQuantity, setCartQuantity] = useState(0);
-  const [cartModal, setCartModal] = useState(-1);
+  const [isCartModalOpen, setIsCartModalOpen] = useState(false);
+  const [cartInfo, setCartInfo] = useState({});
 
   const handleShowCart = () => {
-    let state = cartModal;
-    state === -1 ? (state = 1) : (state = -1);
-    setCartModal(state);
-  }
-  const updateCartQuantity = () => {
-    let quantity = 0;
-    for (let i = 0; i < localStorage.length; i++) {
-      if (localStorage.key(i).includes("amount")) {
-        quantity += parseInt(localStorage.getItem(localStorage.key(i)));
-      }
-    }
+    setIsCartModalOpen((prev) => !prev);
+  };
+
+  const updateCartInfo = () => {
+    const cartData = JSON.parse(localStorage.getItem("cartProduct1"));
+    const quantity = JSON.parse(localStorage.getItem("cartProduct1.amount"));
     setCartQuantity(quantity);
+    setCartInfo(cartData || {});
   };
 
   useEffect(() => {
-    updateCartQuantity();
-    window.addEventListener("click", updateCartQuantity);
+    updateCartInfo();
+    window.addEventListener("click", updateCartInfo);
     return () => {
-      window.removeEventListener("click", updateCartQuantity);
+      window.removeEventListener("click", updateCartInfo);
     };
   }, []);
 
   return (
     <div>
-      <button onClick={() => handleShowCart()} className={styles.cart}>
+      <button onClick={handleShowCart} className={styles.cart}>
         <img src={cart} alt="cart" />
 
         {cartQuantity > 0 && cartQuantity <= 9 && (
           <span className={styles.cart__count}>{cartQuantity}</span>
         )}
         {cartQuantity > 9 && <span className={styles.cart__count}>9+</span>}
-
       </button>
-      {cartModal === 1 && (<CartModal />)}
-      
+
+      {isCartModalOpen && (
+        <CartModal
+          cartInfo={cartInfo}
+          cartQuantity={cartQuantity}
+          closeModal={() => setIsCartModalOpen(false)}
+        />
+      )}
     </div>
   );
-  
 }
+
 export default Cart;
--- src/components/layout/CartModal.jsx
+++ src/components/layout/CartModal.jsx
@@ -1,22 +1,54 @@
-import { useEffect, useState } from "react";
+/* eslint-disable react/prop-types */
+import trash from "../../assets/icon-delete.svg";
+import { useRef, useEffect } from "react";
 import styles from "./CartModal.module.css";
 
-function CartModal() {
-  const [cartInfo, setCartInfo] = useState([]);
+function CartModal({ cartInfo, cartQuantity, closeModal }) {
+  const modalRef = useRef(null);
+
+  const handleClickOutside = (event) => {
+    if (modalRef.current && !modalRef.current.contains(event.target)) {
+      closeModal();
+    }
+  };
+
   useEffect(() => {
-    setCartInfo(JSON.parse(localStorage.getItem("cartProduct1")));
+    document.addEventListener("mouseup", handleClickOutside);
+    return () => {
+      document.removeEventListener("mouseup", handleClickOutside);
+    };
   }, []);
 
-  console.log(cartInfo)
   return (
-    <div id="CartModal" className={styles.cart__modal}>
-      <h3>Cart</h3>
-      {localStorage.length > 0 ? (
-          <div>
+    <div className={styles.modal__overlay}>
+      <div id="CartModal" className={styles.cart__modal} ref={modalRef}>
+        <h3>Cart</h3>
+        {cartQuantity > 0 ? (
+          <div className={styles.cart__container}>
+            <div className={styles.cart__info}>
+              <img className={styles.img__preview} src={cartInfo.img[0]} alt="cart" />
+              <div className={styles.container__details}>
+                <p>{cartInfo.name}</p>
+                <div className={styles.cart__pricing}>
+                  <p>${(cartInfo.price * cartInfo.discount) / 100}.00</p>
+                  <p>x {cartQuantity}</p>
+                  <p className={styles.total}>
+                    ${""}
+                    {((cartInfo.price * cartInfo.discount) / 100) *
+                      cartQuantity}.00
+                  </p>
+                </div>
+              </div>
+              <button className={styles.trash}>
+                <img src={trash} alt="trash" />
+              </button>
+            </div>
+            <button className={styles.checkout__btn}>Checkout</button>
           </div>
         ) : (
-        <p className={styles.empty__cart}>Your cart is empty.</p>
-      )}
+          <p className={styles.empty__cart}>Your cart is empty.</p>
+        )}
+      </div>
     </div>
   );
 }
